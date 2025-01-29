0ab355eb3e05fcb3102761d7cb805f50d7fcf500
Quentin <quentin.farret@gmail.com>
2025-01-29 23:17:20 +0100

    correction de quelque trucs

--- automate.cpp
+++ automate.cpp
@@ -1,48 +1,55 @@
 // Automate.cpp
-#include "Automate.h"
-#include "Etats.h"
+#include "automate.h"
+#include "etats.h"
 #include <iostream>
 #include <vector>
 #include <cassert>
 
-Automate::~Automate() {
+Automate::~Automate()
+{
    // Nettoyage des états
-   while (!stateStack.empty()) {
+   while (!stateStack.empty())
+   {
       delete stateStack.top();
       stateStack.pop();
    }
    // Nettoyage des symboles
-   while (!symbolStack.empty()) {
+   while (!symbolStack.empty())
+   {
       delete symbolStack.top();
       symbolStack.pop();
    }
 }
 
-void Automate::run() {
+void Automate::run()
+{
    // On empile l'état initial (Etat0)
    stateStack.push(new Etat0);
 
-   while (!finished) {
+   while (!finished)
+   {
       // Symbole courant lu par le lexer
-      Symbole * s = getNextSymbole();
+      Symbole *s = getNextSymbole();
 
       // On appelle la méthode transition de l'état au sommet
-      Etat * e = stateStack.top();
+      Etat *e = stateStack.top();
       bool res = e->transition(*this, s);
 
-      if (!res) {
+      if (!res)
+      {
          // Erreur de parsing
          std::cerr << "[ERREUR] Symbole inattendu : ";
-         s->Affiche(); 
+         s->Affiche();
          std::cerr << std::endl;
          finished = true; // On stoppe
       }
    }
 }
 
-void Automate::decalage(Symbole* s, Etat* e) {
+void Automate::decalage(Symbole *s, Etat *e)
+{
    std::cout << "[SHIFT] ";
-   s->Affiche(); 
+   s->Affiche();
    std::cout << " -> " << e->getName() << std::endl;
 
    // Empilage du symbole
@@ -53,23 +60,34 @@ void Automate::decalage(Symbole* s, Etat* e) {
    stateStack.push(e);
 }
 
-void Automate::reduction(int nbSymboles, ReductionRule r) {
+void Automate::reduction(int nbSymboles, ReductionRule r)
+{
    std::cout << "[REDUCE] Règle ";
-   switch(r) {
-      case R_E_VAL:      std::cout << "E->val";    break;
-      case R_E_PAR_E:    std::cout << "E->(E)";    break;
-      case R_E_E_PLUS_E: std::cout << "E->E+E";    break;
-      case R_E_E_MULT_E: std::cout << "E->E*E";    break;
+   switch (r)
+   {
+   case R_E_VAL:
+      std::cout << "E->val";
+      break;
+   case R_E_PAR_E:
+      std::cout << "E->(E)";
+      break;
+   case R_E_E_PLUS_E:
+      std::cout << "E->E+E";
+      break;
+   case R_E_E_MULT_E:
+      std::cout << "E->E*E";
+      break;
    }
    std::cout << std::endl;
 
    // On récupère les symboles à dépiler
-   std::vector<Symbole*> symbolsToReduce;
+   std::vector<Symbole *> symbolsToReduce;
    // On dépile nbSymboles
-   for (int i = 0; i < nbSymboles; i++) {
-      Symbole* topSym = symbolStack.top();
+   for (int i = 0; i < nbSymboles; i++)
+   {
+      Symbole *topSym = symbolStack.top();
       symbolStack.pop();
-      Etat* topState = stateStack.top();
+      Etat *topState = stateStack.top();
       stateStack.pop();
       delete topState; // on libère l'état
 
@@ -77,56 +95,58 @@ void Automate::reduction(int nbSymboles, ReductionRule r) {
    }
 
    // Calcul du nouveau symbole de type E (avec la valeur calculée)
-   Symbole* newSymbol = nullptr;
+   Symbole *newSymbol = nullptr;
 
-   switch(r) {
-      case R_E_VAL:
-      {
-         // E -> val (INT)
-         // Il y a exactement 1 symbole : l'Entier
-         Entier* val = dynamic_cast<Entier*>(symbolsToReduce[0]);
-         assert(val != nullptr);
-         newSymbol = new Expr(val->valeur);
-         break;
-      }
-      case R_E_PAR_E:
-      {
-         // E -> ( E ) : 3 symboles : ')' 'E' '(' en ordre de dépilement
-         // Or dans la pile, l'ordre est l'inverse de l'entrée : topSym = ')', puis E, puis '('
-         // On veut la valeur de E
-         Expr* exprInterne = dynamic_cast<Expr*>(symbolsToReduce[1]);
-         assert(exprInterne != nullptr);
-         newSymbol = new Expr(exprInterne->getValeur());
-         break;
-      }
-      case R_E_E_PLUS_E:
-      {
-         // E -> E + E : on a dépilé dans l'ordre E (droite), +, E (gauche)
-         Expr* right = dynamic_cast<Expr*>(symbolsToReduce[0]);
-         Symbole* plus = symbolsToReduce[1]; // on n'a pas forcément besoin du plus
-         Expr* left  = dynamic_cast<Expr*>(symbolsToReduce[2]);
-         assert(left != nullptr && right != nullptr);
+   switch (r)
+   {
+   case R_E_VAL:
+   {
+      // E -> val (INT)
+      // Il y a exactement 1 symbole : l'Entier
+      Entier *val = dynamic_cast<Entier *>(symbolsToReduce[0]);
+      assert(val != nullptr);
+      newSymbol = new Expr(val->valeur);
+      break;
+   }
+   case R_E_PAR_E:
+   {
+      // E -> ( E ) : 3 symboles : ')' 'E' '(' en ordre de dépilement
+      // Or dans la pile, l'ordre est l'inverse de l'entrée : topSym = ')', puis E, puis '('
+      // On veut la valeur de E
+      Expr *exprInterne = dynamic_cast<Expr *>(symbolsToReduce[1]);
+      assert(exprInterne != nullptr);
+      newSymbol = new Expr(exprInterne->getValeur());
+      break;
+   }
+   case R_E_E_PLUS_E:
+   {
+      // E -> E + E : on a dépilé dans l'ordre E (droite), +, E (gauche)
+      Expr *right = dynamic_cast<Expr *>(symbolsToReduce[0]);
+      Symbole *plus = symbolsToReduce[1]; // on n'a pas forcément besoin du plus
+      Expr *left = dynamic_cast<Expr *>(symbolsToReduce[2]);
+      assert(left != nullptr && right != nullptr);
 
-         int res = left->getValeur() + right->getValeur();
-         newSymbol = new Expr(res);
-         break;
-      }
-      case R_E_E_MULT_E:
-      {
-         // E -> E * E : on a dépilé dans l'ordre E (droite), *, E (gauche)
-         Expr* right = dynamic_cast<Expr*>(symbolsToReduce[0]);
-         Symbole* mult = symbolsToReduce[1];
-         Expr* left  = dynamic_cast<Expr*>(symbolsToReduce[2]);
-         assert(left != nullptr && right != nullptr);
+      int res = left->getValeur() + right->getValeur();
+      newSymbol = new Expr(res);
+      break;
+   }
+   case R_E_E_MULT_E:
+   {
+      // E -> E * E : on a dépilé dans l'ordre E (droite), *, E (gauche)
+      Expr *right = dynamic_cast<Expr *>(symbolsToReduce[0]);
+      Symbole *mult = symbolsToReduce[1];
+      Expr *left = dynamic_cast<Expr *>(symbolsToReduce[2]);
+      assert(left != nullptr && right != nullptr);
 
-         int res = left->getValeur() * right->getValeur();
-         newSymbol = new Expr(res);
-         break;
-      }
+      int res = left->getValeur() * right->getValeur();
+      newSymbol = new Expr(res);
+      break;
+   }
    }
 
    // On supprime les anciens symboles
-   for (auto sym : symbolsToReduce) {
+   for (auto sym : symbolsToReduce)
+   {
       delete sym;
    }
 
@@ -137,7 +157,8 @@ void Automate::reduction(int nbSymboles, ReductionRule r) {
    stateStack.top()->transition(*this, newSymbol);
 }
 
-void Automate::accepter() {
+void Automate::accepter()
+{
    finished = true;
    accepted = true;
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <vector>
-#include "Automate.h"
+#include "automate.h"
 #include "symbole.h" // Pour faire un cast si besoin
 
-int main() {
+// need to put --std=c++11 when compiling with following command : g++ --std=c++11 main.cpp automate.cpp etats.cpp lexer.cpp symbole.cpp -o executeAutomate
+
+int main()
+{
    // Liste d'expressions à tester
    std::vector<std::string> expressions = {
-      "(1+34)*123",    // valide
-      "(1+2)*3+4",     // valide
-      "(1+2*3)",       // valide
-      "((4+5)*6)",     // valide
-      "(42)",          // valide
-      "1+2*3",         // valide
-      "1+*3",          // invalide (symbole * inattendu)
-      "((1+2)",        // invalide (parenthèse fermante manquante)
-      "(1+2))",        // invalide (parenthèse en trop)
-      "(1+(2*3)"       // invalide (parenthèse fermante manquante)
+       "(1+34)*123", // valide
+       "(1+2)*3+4",  // valide
+       "(1+2*3)",    // valide
+       "((4+5)*6)",  // valide
+       "(42)",       // valide
+       "1+2*3",      // valide
+       "1+*3",       // invalide (symbole * inattendu)
+       "((1+2)",     // invalide (parenthèse fermante manquante)
+       "(1+2))",     // invalide (parenthèse en trop)
+       "(1+(2*3)"    // invalide (parenthèse fermante manquante)
    };
 
-   for (auto & expr : expressions) {
+   for (auto &expr : expressions)
+   {
       std::cout << "-------------------------------\n";
       std::cout << "Analyse de : " << expr << "\n";
       std::cout << "-------------------------------\n";
@@ -27,20 +31,26 @@ int main() {
       Automate automate(expr);
       automate.run();
 
-      if (automate.isAccepted()) {
+      if (automate.isAccepted())
+      {
          // Le sommet de la pile contient l'expression finale
-         std::stack<Symbole*> & pile = automate.getSymbolStack();
-         if (!pile.empty()) {
-            Symbole* topSym = pile.top();
-            if ((int)*topSym == E) {
-               Expr* exprFinal = dynamic_cast<Expr*>(topSym);
-               if (exprFinal) {
-                  std::cout << "Resultat du calcul = " 
+         std::stack<Symbole *> &pile = automate.getSymbolStack();
+         if (!pile.empty())
+         {
+            Symbole *topSym = pile.top();
+            if ((int)*topSym == E)
+            {
+               Expr *exprFinal = dynamic_cast<Expr *>(topSym);
+               if (exprFinal)
+               {
+                  std::cout << "Resultat du calcul = "
                             << exprFinal->getValeur() << "\n";
                }
             }
          }
-      } else {
+      }
+      else
+      {
          std::cout << "L'expression est invalide.\n";
       }
 
